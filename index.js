b440412134ea3a33ad97ecf9179e045d9490784c
Vladislove <vlad141098@mail.ru>
2023-11-15 18:12:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const arr = ['a', 'b', 'c'];
+
+arr[10] = '3456';
+
+const arrObj = {
+    a: 'a',
+    1: 'b',
+    2: 'c',
+    abc: {
+        def: {
+            df: [{}, {}]
+        }
+    }
+};
+const b = 'b';
+//arrObj.b = '1234';
+arrObj[b] = '1234';
+
+console.log(arr);
+console.log(arrObj['b']);
+console.log(arrObj.b);
+console.log(arrObj);
+console.log(b);
+//const obj = { a: 1, b: 2 };
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
---
+++ script.js
@@ -0,0 +1,16 @@
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 10);
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какая ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof (answers));
+console.log(typeof (null));
